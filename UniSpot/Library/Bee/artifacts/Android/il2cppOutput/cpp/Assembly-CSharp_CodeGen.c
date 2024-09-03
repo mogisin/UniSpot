c57f1ed907b3092a6bb5d79ed141f316e661beb2
mogisin <sukjk789@naver.com>
2024-09-03 16:43:09 +0900

    Feat: Reset3

---
+++ UniSpot/Library/Bee/artifacts/Android/il2cppOutput/cpp/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,77 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern void ImageTracker_Awake_mF0965772758938187CDDE525DCB020EAD0EAF5C1 (void);
+extern void ImageTracker_OnEnable_mF68EC247C896E301D5F8A2C928EFD898364035BE (void);
+extern void ImageTracker_OnDisable_m423865D8E1B2D7AE7367F97852DFC82DBAEE5631 (void);
+extern void ImageTracker_OnTrackedImagesChanged_mD4D80C935F06FD601CB6B77BD956D686DA05192E (void);
+extern void ImageTracker__ctor_m713E662C7F699779787AF5C331ED1A8F32E53856 (void);
+extern void GameQuit_GameExit_m43B6BCB2965CBDEC240CB0EB5103E8DB8B21B557 (void);
+extern void GameQuit__ctor_mD028D71B8ACF93E33026FE59E72812A4C528DDBA (void);
+extern void SceneManage_MoveToMain_mA54CBAF7881A25C3CD93216579EA957235E598C6 (void);
+extern void SceneManage_MoveToSpot1_m036826F03B2F80DB5D6E29ECE0A7131981703CE4 (void);
+extern void SceneManage__ctor_mE9BDA9D7A724260684866D5A570A65E3D132FF70 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033 (void);
+extern void UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1 (void);
+static Il2CppMethodPointer s_methodPointers[12] = 
+{
+	ImageTracker_Awake_mF0965772758938187CDDE525DCB020EAD0EAF5C1,
+	ImageTracker_OnEnable_mF68EC247C896E301D5F8A2C928EFD898364035BE,
+	ImageTracker_OnDisable_m423865D8E1B2D7AE7367F97852DFC82DBAEE5631,
+	ImageTracker_OnTrackedImagesChanged_mD4D80C935F06FD601CB6B77BD956D686DA05192E,
+	ImageTracker__ctor_m713E662C7F699779787AF5C331ED1A8F32E53856,
+	GameQuit_GameExit_m43B6BCB2965CBDEC240CB0EB5103E8DB8B21B557,
+	GameQuit__ctor_mD028D71B8ACF93E33026FE59E72812A4C528DDBA,
+	SceneManage_MoveToMain_mA54CBAF7881A25C3CD93216579EA957235E598C6,
+	SceneManage_MoveToSpot1_m036826F03B2F80DB5D6E29ECE0A7131981703CE4,
+	SceneManage__ctor_mE9BDA9D7A724260684866D5A570A65E3D132FF70,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1_Get_mBEB95BEB954BB63E9710BBC7AD5E78C4CB0A0033,
+	UnitySourceGeneratedAssemblyMonoScriptTypes_v1__ctor_mE70FB23ACC1EA12ABC948AA22C2E78B2D0AA39B1,
+};
+static const int32_t s_InvokerIndices[12] = 
+{
+	6580,
+	6580,
+	6580,
+	5050,
+	6580,
+	6580,
+	6580,
+	6580,
+	6580,
+	6580,
+	10021,
+	6580,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	12,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+};
