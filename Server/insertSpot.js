2567cd96f4de5ab19157366170352edf9b802dc3
witdory <yoonbal0406@naver.com>
2024-10-03 00:49:11 +0900

    중앙도서관 스팟 추가

--- Server/insertSpot.js
+++ Server/insertSpot.js
@@ -11,15 +11,18 @@ if (!mongoURI) {
 }
 
 // 삽입할 데이터 정의
+// const spotsData = {
+//   '소프트웨어융합대학': { latitude: 37.239603, longitude: 127.083157 },
+//   '전자정보대학': { latitude: 37.239782, longitude: 127.083313 },
+//   '응용과학대학': { latitude: 37.239811, longitude: 127.083476 },
+//   '체육대학': { latitude: 37.244493, longitude: 127.080436 },
+//   '공과대학': { latitude: 37.246468, longitude: 127.080844 },
+//   '예술디자인대학': { latitude: 37.241709, longitude: 127.084441 },
+//   '외국어대학': { latitude: 37.245391, longitude: 127.077649 },
+//   '생명과학대학': { latitude: 37.242962, longitude: 127.080932 }
+// };
 const spotsData = {
-  '소프트웨어융합대학': { latitude: 37.239603, longitude: 127.083157 },
-  '전자정보대학': { latitude: 37.239782, longitude: 127.083313 },
-  '응용과학대학': { latitude: 37.239811, longitude: 127.083476 },
-  '체육대학': { latitude: 37.244493, longitude: 127.080436 },
-  '공과대학': { latitude: 37.246468, longitude: 127.080844 },
-  '예술디자인대학': { latitude: 37.241709, longitude: 127.084441 },
-  '외국어대학': { latitude: 37.245391, longitude: 127.077649 },
-  '생명과학대학': { latitude: 37.242962, longitude: 127.080932 }
+  '중앙도서관': {latitude:37.241047, longitude:127.079659 }
 };
 
 // 비동기 함수로 데이터 삽입
