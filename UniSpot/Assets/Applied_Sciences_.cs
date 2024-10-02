9c9691a92491da137e28ccbc11f7928970ecb52f
mogisin <sukjk789@naver.com>
2024-10-02 21:47:54 +0900

    Test: Add Prefab Anim

---
+++ UniSpot/Assets/Applied_Sciences_.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Applied_Sciences_ : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Applied_Sciences_1_anim()
+    {
+        // 애니메이션 이벤트에서 실행하려는 로직
+        Debug.Log("Applied_Sciences_1_anim 이벤트가 호출되었습니다.");
+    }
+}
