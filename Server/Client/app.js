6487c2a67b5ae3e58c2ad7fbca992a84805bce26
witdory <yoonbal0406@naver.com>
2024-10-02 22:17:53 +0900

    리팩토링: 클라이언트 js 코드 모듈화

--- Server/Client/app.js
+++ Server/Client/app.js
@@ -1,78 +1,58 @@
-function initWebSocket() {
-    const socket = new WebSocket('ws://localhost:8000');
-    let isMarkersVisible = false; // 몬스터 마커가 표시 중인지 추적
-    let isCapturePending = false; // 캡처 중 여부를 확인하는 플래그
+import { MapManager } from './map.js';
 
-    // 로그 출력 함수
-    function logMessage(message) {
+export class WebSocketManager {
+    constructor(url, mapManager) {
+        this.socket = new WebSocket(url);
+        this.mapManager = mapManager;
+        this.isMarkersVisible = false;
+        this.isCapturePending = false;
+        this.initWebSocket();
+    }
+
+    logMessage(message) {
         const logDiv = document.getElementById('log');
         logDiv.innerHTML += `<p>${message}</p>`;
         logDiv.scrollTop = logDiv.scrollHeight;
     }
 
-    // WebSocket 연결이 열리면 실행
-    socket.onopen = function() {
-        logMessage('WebSocket connection opened.');
-    };
-
-    // 서버로부터 메시지 수신
-    socket.onmessage = function(event) {
-        const data = JSON.parse(event.data);
-
-        if (data.type === 'get_all_monsters') {
-            if (isMarkersVisible) {
-                showAllMonsterMarkers(data.monsters); // 몬스터 마커를 보여줌
-            }
-
-            // 랜덤 몬스터 포획 대기 중이면 처리
-            if (isCapturePending) {
-                const monsters = data.monsters;
-                if (monsters.length > 0) {
-                    const randomMonster = monsters[Math.floor(Math.random() * monsters.length)];
-                    const captureMessage = {
-                        type: 'capture',
-                        username: 'testUser',
-                        monsterId: randomMonster._id
-                    };
-                    socket.send(JSON.stringify(captureMessage));
-                    logMessage(`Requested to capture monster with ID: ${randomMonster._id}`);
-                } else {
-                    logMessage('No monsters available to capture.');
-                }
-                isCapturePending = false; // 캡처 완료 후 플래그 초기화
-            }
-        } else if (data.type === 'delete_all_success') {
-            logMessage('All monsters have been deleted from the database.');
-            removeAllMonsterMarkers(); // 모든 마커 제거
-        }
-
-        logMessage('Received from server: ' + event.data);
-    };
-
-    // WebSocket 연결이 닫힐 때
-    socket.onclose = function() {
-        logMessage('WebSocket connection closed.');
-    };
-
-    // WebSocket 에러 발생 시
-    socket.onerror = function(error) {
-        logMessage('WebSocket error: ' + error.message);
-    };
-
-    // 위치 정보 전송 함수
-    window.sendLocation = function(department) {
-        const departmentCoordinates = {
-            '소프트웨어융합대학': { latitude: 37.239603, longitude: 127.083157 },
-            '전자정보대학': { latitude: 37.239782, longitude: 127.083313 },
-            '응용과학대학': { latitude: 37.239811, longitude: 127.083476 },
-            '체육대학': { latitude: 37.244493, longitude: 127.080436 },
-            '공과대학': { latitude: 37.246468, longitude: 127.080844 },
-            '예술디자인대학': { latitude: 37.241709, longitude: 127.084441 },
-            '외국어대학': { latitude: 37.245391, longitude: 127.077649 },
-            '생명과학대학': { latitude: 37.242962, longitude: 127.080932 }
+    initWebSocket() {
+        this.socket.onopen = () => {
+            this.logMessage('WebSocket connection opened.');
         };
 
-        const location = departmentCoordinates[department];
+        this.socket.onmessage = (event) => {
+            const data = JSON.parse(event.data);
+            this.handleMessage(data);
+        };
+
+        this.socket.onclose = () => {
+            this.logMessage('WebSocket connection closed.');
+        };
+
+        this.socket.onerror = (error) => {
+            this.logMessage('WebSocket error: ' + error.message);
+        };
+    }
+
+    handleMessage(data) {
+        if (data.type === 'get_all_monsters') {
+            if (this.isMarkersVisible) {
+                this.showAllMonsterMarkers(data.monsters);
+            }
+
+            if (this.isCapturePending) {
+                this.captureRandomMonster(data.monsters);
+            }
+        } else if (data.type === 'delete_all_success') {
+            this.logMessage('All monsters have been deleted from the database.');
+            this.mapManager.removeAllMonsterMarkers();
+        }
+
+        this.logMessage('Received from server: ' + JSON.stringify(data));
+    }
+
+    sendLocation(department) {
+        const location = this.mapManager.departmentCoordinates[department];
         if (location) {
             const message = {
                 type: 'location',
@@ -80,63 +60,71 @@ function initWebSocket() {
                 latitude: location.latitude,
                 longitude: location.longitude
             };
-            socket.send(JSON.stringify(message));
-            logMessage('Sent location for ' + department + ': ' + JSON.stringify(message));
+            this.socket.send(JSON.stringify(message));
+            this.logMessage('Sent location for ' + department + ': ' + JSON.stringify(message));
         }
-    };
+    }
 
-    // 랜덤 몬스터 생성 요청
-    window.generateMonsters = function() {
+    generateMonsters() {
         const message = {
             type: 'generate_monsters',
-            count: 1 // 생성할 몬스터 수
+            count: 1
         };
-        socket.send(JSON.stringify(message));
-        logMessage('Requested to generate random monsters: ' + JSON.stringify(message));
-    };
+        this.socket.send(JSON.stringify(message));
+        this.logMessage('Requested to generate random monsters.');
+    }
 
-    // 모든 몬스터의 위치 요청
-    window.getAllMonsters = function() {
+    getAllMonsters() {
         const message = {
             type: 'get_all_monsters'
         };
-        socket.send(JSON.stringify(message));
-        logMessage('Requested to get all monsters.');
-    };
+        this.socket.send(JSON.stringify(message));
+        this.logMessage('Requested to get all monsters.');
+    }
 
-    // 모든 몬스터 삭제 요청
-    window.deleteAllMonsters = function() {
+    deleteAllMonsters() {
         const message = {
             type: 'delete_all'
         };
-        socket.send(JSON.stringify(message));
-        logMessage('Requested to delete all monsters.');
-    };
-
-    // 랜덤 몬스터 포획 요청
-    window.captureAnyMonster = function() {
-        logMessage('captureAnyMonster button clicked');
-        isCapturePending = true; // 캡처 요청이 대기 중임을 표시
-        getAllMonsters(); // 모든 몬스터 요청
-    };
-
-    // 마커 모두 제거 (getAllMonsters로 받은 데이터를 활용)
-    function showAllMonsterMarkers(monsters) {
-        monsters.forEach(monster => {
-            addMonsterMarker(monster._id,monster.name, monster.location.latitude, monster.location.longitude);
-        });
-        logMessage('All monster markers added.');
+        this.socket.send(JSON.stringify(message));
+        this.logMessage('Requested to delete all monsters.');
     }
 
-    // 토글 버튼 클릭 시 실행되는 함수
-    window.toggleMonsterMarkers = function() {
-        if (isMarkersVisible) {
-            // 마커 숨기기
-            removeAllMonsterMarkers();
+    captureAnyMonster() {
+        this.logMessage('captureAnyMonster button clicked');
+        this.isCapturePending = true;
+        this.getAllMonsters();
+    }
+
+    showAllMonsterMarkers(monsters) {
+        monsters.forEach(monster => {
+            this.mapManager.addMonsterMarker(monster._id, monster.name, monster.location.latitude, monster.location.longitude);
+        });
+        this.logMessage('All monster markers added.');
+    }
+
+    toggleMonsterMarkers() {
+        if (this.isMarkersVisible) {
+            this.mapManager.removeAllMonsterMarkers();
         } else {
-            // 마커 보여주기
-            getAllMonsters();
+            this.getAllMonsters();
         }
-        isMarkersVisible = !isMarkersVisible; // 상태 변경
-    };
+        this.isMarkersVisible = !this.isMarkersVisible;
+    }
+
+    captureRandomMonster(monsters) {
+        if (monsters.length > 0) {
+            const randomMonster = monsters[Math.floor(Math.random() * monsters.length)];
+            const captureMessage = {
+                type: 'capture',
+                username: 'testUser',
+                monsterId: randomMonster._id
+            };
+            this.socket.send(JSON.stringify(captureMessage));
+            this.logMessage(`Requested to capture monster with ID: ${randomMonster._id}`);
+        } else {
+            this.logMessage('No monsters available to capture.');
+        }
+        this.isCapturePending = false;
+    }
 }
--- Server/Client/map.js
+++ Server/Client/map.js
@@ -1,69 +1,64 @@
-let map;  // 전역 변수로 선언
-let monsterMarkers = {};  // 몬스터 마커들을 저장할 객체
+export class MapManager {
+    constructor(mapElementId) {
+        this.mapElementId = mapElementId;
+        this.map = null;
+        this.monsterMarkers = {};
+        this.departmentCoordinates = {
+            '소프트웨어융합대학': { latitude: 37.239603, longitude: 127.083157 },
+            '전자정보대학': { latitude: 37.239782, longitude: 127.083313 },
+            '응용과학대학': { latitude: 37.239811, longitude: 127.083476 },
+            '체육대학': { latitude: 37.244493, longitude: 127.080436 },
+            '공과대학': { latitude: 37.246468, longitude: 127.080844 },
+            '예술디자인대학': { latitude: 37.241709, longitude: 127.084441 },
+            '외국어대학': { latitude: 37.245391, longitude: 127.077649 },
+            '생명과학대학': { latitude: 37.242962, longitude: 127.080932 }
+        };
+    }
 
-window.initMap = function() {
-    // 지도를 초기화
-    map = new google.maps.Map(document.getElementById("map"), {
-        center: { lat: 37.243056, lng: 127.080063 },
-        zoom: 16
-    });
-
-    const departmentCoordinates = {
-        '소프트웨어융합대학': { latitude: 37.239603, longitude: 127.083157 },
-        '전자정보대학': { latitude: 37.239782, longitude: 127.083313 },
-        '응용과학대학': { latitude: 37.239811, longitude: 127.083476 },
-        '체육대학': { latitude: 37.244493, longitude: 127.080436 },
-        '공과대학': { latitude: 37.246468, longitude: 127.080844 },
-        '예술디자인대학': { latitude: 37.241709, longitude: 127.084441 },
-        '외국어대학': { latitude: 37.245391, longitude: 127.077649 },
-        '생명과학대학': { latitude: 37.242962, longitude: 127.080932 }
-    };
-
-    const bounds = new google.maps.LatLngBounds();
-
-    // 학과 위치에 마커 추가
-    Object.keys(departmentCoordinates).forEach(department => {
-        const coordinates = departmentCoordinates[department];
-        
-        const marker = new google.maps.Marker({
-            position: { lat: coordinates.latitude, lng: coordinates.longitude },
-            label: department,
-            map: map
+    initMap() {
+        this.map = new google.maps.Map(document.getElementById(this.mapElementId), {
+            center: { lat: 37.243056, lng: 127.080063 },
+            zoom: 16
         });
-        bounds.extend(marker.position);
-    });
 
-    map.fitBounds(bounds);
+        const bounds = new google.maps.LatLngBounds();
 
-    // 몬스터 마커를 추가하는 함수
-    window.addMonsterMarker = function(monsterId,monsterName, latitude, longitude) {
-        // 이미 해당 몬스터 마커가 있는지 확인
-        if (!monsterMarkers[monsterId]) {
+        // 학과 위치에 마커 추가
+        Object.keys(this.departmentCoordinates).forEach(department => {
+            const coordinates = this.departmentCoordinates[department];
+
+            const marker = new google.maps.Marker({
+                position: { lat: coordinates.latitude, lng: coordinates.longitude },
+                label: department,
+                map: this.map
+            });
+            bounds.extend(marker.position);
+        });
+
+        this.map.fitBounds(bounds);
+    }
+
+    addMonsterMarker(monsterId, monsterName, latitude, longitude) {
+        if (!this.monsterMarkers[monsterId]) {
             const marker = new google.maps.Marker({
                 position: { lat: latitude, lng: longitude },
                 label: monsterName,
-                map: map
+                map: this.map
             });
-            monsterMarkers[monsterId] = marker;  // 마커 저장
+            this.monsterMarkers[monsterId] = marker;
         }
-    };
+    }
 
-    // 몬스터 마커를 제거하는 함수
-    window.removeMonsterMarker = function(monsterId) {
-        if (monsterMarkers[monsterId]) {
-            monsterMarkers[monsterId].setMap(null);  // 마커를 지도에서 제거
-            delete monsterMarkers[monsterId];  // 저장된 마커 정보 삭제
+    removeMonsterMarker(monsterId) {
+        if (this.monsterMarkers[monsterId]) {
+            this.monsterMarkers[monsterId].setMap(null);
+            delete this.monsterMarkers[monsterId];
         }
-    };
+    }
 
-    // 모든 몬스터 마커를 제거하는 함수
-    window.removeAllMonsterMarkers = function() {
-        Object.keys(monsterMarkers).forEach(monsterId => {
-            monsterMarkers[monsterId].setMap(null);
-            delete monsterMarkers[monsterId];
+    removeAllMonsterMarkers() {
+        Object.keys(this.monsterMarkers).forEach(monsterId => {
+            this.removeMonsterMarker(monsterId);
         });
-    };
-
-    // 맵 초기화 이후 WebSocket 시작
-    initWebSocket();
-};
+    }
+}
